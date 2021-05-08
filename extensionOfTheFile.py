a90ae8353061bfe61d1438eed7f461e5489f768c
ravisaikiran <83855569+ravisaikiran@users.noreply.github.com>
2021-05-08 11:45:29 +0530

    displays the extension of the file

---
+++ extensionOfTheFile.py
@@ -0,0 +1,18 @@
+fileName=input()
+if '.' in fileName:
+    i=fileName.index('.')
+    extension=fileName[i+1:]
+if extension=="py":
+    print("The extension of the file is :")
+    print("python")
+elif extension=="c":
+    print("The extension of the file is :")
+    print
+elif extension=="java":
+    print("The extension of the file is :")
+    print("java")
+elif extension=="cc":
+    print("The extension of the file is :")
+    print("C++ language")
+else:
+    print("some other language")
