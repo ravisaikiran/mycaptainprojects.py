2d5d9b3bcaf2874580bcb2a27334d3434cc780f2
ravisaikiran <83855569+ravisaikiran@users.noreply.github.com>
2021-05-08 12:52:56 +0530

    Update extensionOfTheFile.py

--- extensionOfTheFile.py
+++ extensionOfTheFile.py
@@ -1,20 +1,9 @@
 fileName=input()
+Dict={"py":"python","c":"C","cc":"C++","jsp":"Java","html":"HTML","pl":"Perl","php":"PHP"}
 if '.' in fileName:
     i=fileName.index('.')
     extension=fileName[i+1:]
-    if extension=="py":
-        print("The extension of the file is :")
-        print("python")
-    elif extension=="c":
-        print("The extension of the file is :")
-        print("C language")
-    elif extension=="java":
-        print("The extension of the file is :")
-        print("java")
-    elif extension=="cc":
-        print("The extension of the file is :")
-        print("C++ language")
-    else:
-        print("some other language")
+    print("The extension of the file is :")
+    print(Dict[extension])
 else:
     print("INVALID INPUT")
