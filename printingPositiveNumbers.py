4e271cf3f3a6fdfd3228c01606c26c6c30c8bbdb
ravisaikiran <83855569+ravisaikiran@users.noreply.github.com>
2021-05-08 16:33:19 +0530

    printing Positive Numbers in a list

---
+++ printingPositiveNumbers.py
@@ -0,0 +1,12 @@
+list1=[12, -7, 5, 64, -14]
+output1=[]
+list2=[12, 14, -95, 3]
+output2=[]
+for i in list1:
+    if(i>0):
+        output1.append(i)
+for j in list2:
+    if(j>0):
+        output2.append(j)
+print(output1)
+print(output2)
