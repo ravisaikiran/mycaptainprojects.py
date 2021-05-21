646ae6246944a5f2d50e9453ed8a1bce7cf85fd2
ravisaikiran <83855569+ravisaikiran@users.noreply.github.com>
2021-05-21 21:01:43 +0530

    Update areaofcircle.py

--- areaofcircle.py
+++ areaofcircle.py
@@ -1,6 +1,10 @@
-radius=input()
-r=float(radius)
-pi=22/7
-area=pi*r*r
-print("the area of a circle with radius "+radius+" is :")
-print(area)
+import math
+class circle:
+    def __init__(self):
+        self.radius=float(input("enter the radius :"))
+        self.get_area()
+    def get_area(self):
+        return (self.radius**2)*math.pi
+circle1=circle()
+print(round(circle1.get_area(),2))
+
